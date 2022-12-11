b535b5be37cedd261a91b67aef3dd6af27caf865
jordy-ricardo25 <jordy.ricardo25@outlook.com>
2022-12-11 00:49:33 -0500

    Type "XamlControlsResources" was added 'n' more...

---
+++ MSB.UI (Windows Presentation)/UI/XamlControlsResources.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace MSB.UI
+{
+    /// <summary>
+    /// Default styles for the controls in the library.
+    /// </summary>
+    public sealed class XamlControlsResources : ResourceDictionary
+    {
+        /// <summary>
+        /// Initializes a new instance of the XamlControlsResources class.
+        /// </summary>
+        public XamlControlsResources()
+        {
+            this.Source = new($"{prefix}UI/XamlControlsResources.xaml");
+        }
+
+        const string prefix = "pack://application:,,,/MSB.UI-WPF;component/";
+    }
+}
