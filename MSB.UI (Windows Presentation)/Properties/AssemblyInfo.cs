81a953ad482e661de7762d89543c118979a83a5a
jordy-ricardo25 <jordy.ricardo25@outlook.com>
2022-12-16 15:42:43 -0500

    Added XmlnsPrefix "msb".

--- MSB.UI (Windows Presentation)/Properties/AssemblyInfo.cs
+++ MSB.UI (Windows Presentation)/Properties/AssemblyInfo.cs
@@ -1,3 +1,11 @@
 ﻿using System.Windows;
+using System.Windows.Markup;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+
+[assembly: XmlnsPrefix("http://ui.msb-studios.com/winfx/xaml", "msb")]
+
+[assembly: XmlnsDefinition("http://ui.msb-studios.com/winfx/xaml", "MSB.UI")]
+[assembly: XmlnsDefinition("http://ui.msb-studios.com/winfx/xaml", "MSB.Converters")]
+[assembly: XmlnsDefinition("http://ui.msb-studios.com/winfx/xaml", "MSB.UI.Controls")]
+[assembly: XmlnsDefinition("http://ui.msb-studios.com/winfx/xaml", "MSB.Media.Animations")]
